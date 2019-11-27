0c3de97922b1f91609693c848d83fce7485f8013
shai bachar <shaibachar@gmail.com>
2019-11-27 11:19:42 +0200

    init

---
+++ src/main/java/com/shipitto/shipitto/ShipittoApplication.java
@@ -0,0 +1,13 @@
+package com.shipitto.shipitto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipittoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shipitto/shipitto/ShipittoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipitto.shipitto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
